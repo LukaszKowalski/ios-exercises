1d3bc3bfc04b36a4aa455e8b4c52f838212ea0be
Lukasz Kowalski <lukasz.kowalski91@gmail.com>
2014-07-10 14:35:02 +0200

    Simple Data Types and Objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,34 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    int firstNumber = [number intValue];
+    int secondNumber = 2;
+    NSNumber *result = [NSNumber numberWithInt:firstNumber *secondNumber];
+    return result;
+    
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    int first = MIN(number, otherNumber);
+    int last = MAX(number, otherNumber);
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    for (first; first <= last; first++) {
+        [result addObject:[NSNumber numberWithInt:first]];
+    }
+    return result;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    //NSSortDescriptor *lowestNumber = [[NSSortDescriptor sortDescriptorWithKey:nil ascending:YES];
+    //NSArray *sortedArray = [arrayOfNumbers sortedArrayUsingDescriptors:@[lowestNumber]];
+    //NSArray *sortedResult = [sortedArray sor]
+    //return sortedArray;
+    
+    NSSortDescriptor *lowest = [NSSortDescriptor sortDescriptorWithKey:nil ascending:YES];
+    NSArray *sortResult = [arrayOfNumbers sortedArrayUsingDescriptors:@[lowest]];
+    NSLog(@"%@", sortResult);
+    NSNumber *result = [NSNumber numberWithInteger:[[sortResult firstObject] integerValue]];
+    return [result integerValue];
 }
 
 @end
